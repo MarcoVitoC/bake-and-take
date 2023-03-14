03554b83d8cc810a60e72da33be2e2cbb71c021b
MarcoVitoC <marcovito.chr@gmail.com>
2023-03-14 14:53:13 +0700

    add guest page

---
+++ public/js/script.js
@@ -0,0 +1,51 @@
+$('document').ready(function() {
+  $('.dot2022').show()
+  $('.cake-year2022-img').show()
+  $('.cake-year2021-img').hide()
+  $('.cake-year2020-img').hide()
+  $('.cake-year2019-img').hide()
+
+  $('.cake-year2022').on('click', () => {
+    $('.dot2022').show()
+    $('.dot2021').hide()
+    $('.dot2020').hide()
+    $('.dot2019').hide()
+    $('.cake-year2022-img').show()
+    $('.cake-year2021-img').hide()
+    $('.cake-year2020-img').hide()
+    $('.cake-year2019-img').hide()
+  })
+
+  $('.cake-year2021').on('click', () => {
+    $('.dot2022').hide()
+    $('.dot2021').show()
+    $('.dot2020').hide()
+    $('.dot2019').hide()
+    $('.cake-year2022-img').hide()
+    $('.cake-year2021-img').show()
+    $('.cake-year2020-img').hide()
+    $('.cake-year2019-img').hide()
+  })
+
+  $('.cake-year2020').on('click', () => {
+    $('.dot2022').hide()
+    $('.dot2021').hide()
+    $('.dot2020').show()
+    $('.dot2019').hide()
+    $('.cake-year2022-img').hide()
+    $('.cake-year2021-img').hide()
+    $('.cake-year2020-img').show()
+    $('.cake-year2019-img').hide()
+  })
+
+  $('.cake-year2019').on('click', () => {
+    $('.dot2022').hide()
+    $('.dot2021').hide()
+    $('.dot2020').hide()
+    $('.dot2019').show()
+    $('.cake-year2022-img').hide()
+    $('.cake-year2021-img').hide()
+    $('.cake-year2020-img').hide()
+    $('.cake-year2019-img').show()
+  })
+})
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="{{ asset('/css/navbar.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/guest.css') }}">
+  <title>Bake & Take | Home</title>
+</head>
+<body>
+  @include('components.__navbar')
+  <div>
+    @yield('container')
+  </div>
+</body>
+<script src="{{ asset('jquery.js') }}"></script>
+<script src="{{ asset('js/script.js') }}"></script>
+</html>
