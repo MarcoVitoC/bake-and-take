84438f6b1881c9855559c4ad619f9cc734a20972
MarcoVitoC <marcovito.chr@gmail.com>
2023-03-14 15:31:17 +0700

    social media open link fix with new tab

--- resources/views/guest.blade.php
+++ resources/views/guest.blade.php
@@ -84,10 +84,10 @@
       <p>Tidak ada yang lebih memuaskan daripada kue yang dibuat dengan <br/>hati yang tulus dan cita rasa yang terbaik. Kunjungi toko kami untuk <br/>merasakan nikmatnya kue-kue premium yang hanya bisa Anda <br/>dapatkan di sini.</p>
     </div>
     <div class="social-media">
-      <a class="social-media-icon" href="https://www.facebook.com/"><img src="{{ asset('assets/Home/facebook.png') }}" alt="Facebook"></a>
-      <a class="social-media-icon" href="https://www.instagram.com/"><img src="{{ asset('assets/Home/instagram.png') }}" alt="Instagram"></a>
-      <a class="social-media-icon" href="https://www.linkedin.com/"><img src="{{ asset('assets/Home/linkedin.png') }}" alt="LinkedIn"></a>
-      <a class="social-media-icon" href="https://www.google.com/gmail/about/"><img src="{{ asset('assets/Home/gmail.png') }}" alt="Gmail"></a>
+      <a class="social-media-icon" href="https://www.facebook.com/" target="_blank"><img src="{{ asset('assets/Home/facebook.png') }}" alt="Facebook"></a>
+      <a class="social-media-icon" href="https://www.instagram.com/" target="_blank"><img src="{{ asset('assets/Home/instagram.png') }}" alt="Instagram"></a>
+      <a class="social-media-icon" href="https://www.linkedin.com/" target="_blank"><img src="{{ asset('assets/Home/linkedin.png') }}" alt="LinkedIn"></a>
+      <a class="social-media-icon" href="https://www.google.com/gmail/about/" target="_blank"><img src="{{ asset('assets/Home/gmail.png') }}" alt="Gmail"></a>
     </div>
     <h5>Copyright @2023 Bake & Take</h5>
   </div>
