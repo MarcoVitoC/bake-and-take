635b39dd514e0822a45b19eee55d4f96ea1496ac
JerichoThe <jericho.heroes12@gmail.com>
2023-03-18 21:04:32 +0700

    Update forget password

--- resources/views/forget-password.blade.php
+++ resources/views/forget-password.blade.php
@@ -4,31 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Forget Password</title>
     <link rel="stylesheet" href="{{ asset('css/forget-password.css') }}">
-    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" />   --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" />  
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" /> 
-    <title>Document</title>
+    
 </head>
 <body>
-    <div class="icon-back">
-        
-        
-            <a href="/login"><i class="fa fa-chevron-left fa-lg">  Back</i></a>
-        
-    </div>
-    <div class="body-container">
-        <div class="inside-forget-password-box">
-            
-                <div class="email">
-                    <label class="email-text" for="email">Email</label>
-                    <input type="text" class="email-box" placeholder="Masukan Email Terdaftar....." name="email" value="">
-                    <span class="text-danger">@error('email'){{$message}}@enderror</span>
+    <div class="container">
+        <div class="forget-box">Forget Password</div>
+            <form action="" method="post">
+
+                <div class="inside-container">
+                    <label for="name" class="name">Name: <span class="req">*</span></label>
+                    <input class="text" name="name" type="text" placeholder="Masukan Email Terdaftar...." required>
+                    
+                    
+                    <div class="box"><input type="submit" value="Send" class="button-send"></div>
                 </div>
-                <button class="button-send" type="submit"><a href="/reset-password/req-send" style="text-decoration: none"><div style="color: black">Send</div></a></button>
-            
+            </form>
+
         </div>
-        <div class="forget-password-container"></div>
-        <div class="forget-password-text">Forget Password</div>l
     </div>
+        
+    
 </body>
 </html>
+
+
