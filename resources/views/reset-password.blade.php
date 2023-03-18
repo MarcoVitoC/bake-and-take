e327ccf8ae2d85700a420ef68f2e69797f117bd1
JerichoThe <jericho.heroes12@gmail.com>
2023-03-18 19:51:12 +0700

    Update reset-password

--- resources/views/reset-password.blade.php
+++ resources/views/reset-password.blade.php
@@ -4,18 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-    <link rel="stylesheet" href="{{ asset('css/loginstyle.css') }}">
+    <title>Reset Password</title>
+    <link rel="stylesheet" href="{{ asset('css/reset-password.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" />  
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" /> 
     
 </head>
 <body>
     <div class="container">
-        <div class="login-box">Reset Password</div>
+        <div class="reset-box">Reset Password</div>
         <div class="inside-container">
             <div class="box">
-                <div class="icon"><i class="fa fa-user-circle fa-lg" ></i></div>
+                <div class="icon"><i class="fa fa-key fa-lg" ></i></div>
                 <input class="text" type="text" placeholder="Password" >
             </div>
             <div style="height: 10px;"></div>
@@ -24,7 +24,7 @@
                 <input class="text" type="text" placeholder="Confirm Password" >
             </div>
                 
-            <div class="box"><div class="button-login">Reset</div></div>
+            <div class="box"><div class="button-reset">RESET</div></div>
             
             
         </div>
