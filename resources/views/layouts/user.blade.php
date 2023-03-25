cebb736dd8611ec8bb73e963c70f377adb9a5c41
Marco Vito Christopher <95926467+MarcoVitoC@users.noreply.github.com>
2023-03-25 20:31:53 +0700

    Update user.blade.php

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -7,6 +7,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap"/> --}}
   <link rel="stylesheet" href="{{ asset('/css/navbar.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/user.css') }}">
   <link rel="stylesheet" href="{{ asset('/css/transaction.css') }}">
   <link rel="stylesheet" href="{{ asset('/css/footer.css') }}">
   <title>Bake & Take | {{ $title }}</title>
