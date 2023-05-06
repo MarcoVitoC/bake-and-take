c73af3e8b31908398a4e9827d5ba44c39cca0c29
MarcoVitoC <marcovito.chr@gmail.com>
2023-05-06 15:10:52 +0700

    fix bug in favorite feature

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 
 class GuestController extends Controller
 {
-    public function index () {
-        return view('guest.home', [
-            'title' => 'Guest'
-        ]);
-    }
+   public function index () {
+      return view('guest.home', [
+         'title' => 'Guest'
+      ]);
+   }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,54 +11,54 @@ Route::post('/', [LoginController::class, 'logout']);
 
 // GUEST
 Route::middleware(['guest'])->group(function() {
-    Route::get('/', [GuestController::class, 'index']);
-    Route::get('/login', [LoginController::class, 'index'])->name('login');
-    Route::post('/login', [LoginController::class, 'authentication']);
-    Route::get('/login/forgot-password', [LoginController::class, 'showForgotPassword']);
-    Route::post('/login/forgot-password', [LoginController::class, 'forgotPassword']);
-    Route::get('/reset-password/{id}', [LoginController::class, 'showResetPassword'])->name('resetPassword');
-    Route::put('/reset-password/{id}', [LoginController::class, 'resetPassword']);
-    Route::get('/reset-password-success', [LoginController::class, 'resetPasswordSuccess']);
-    Route::get('/register', [RegisterController::class, 'index']);
-    Route::post('/register', [RegisterController::class, 'register']);
+   Route::get('/', [GuestController::class, 'index']);
+   Route::get('/login', [LoginController::class, 'index'])->name('login');
+   Route::post('/login', [LoginController::class, 'authentication']);
+   Route::get('/login/forgot-password', [LoginController::class, 'showForgotPassword']);
+   Route::post('/login/forgot-password', [LoginController::class, 'forgotPassword']);
+   Route::get('/reset-password/{id}', [LoginController::class, 'showResetPassword'])->name('resetPassword');
+   Route::put('/reset-password/{id}', [LoginController::class, 'resetPassword']);
+   Route::get('/reset-password-success', [LoginController::class, 'resetPasswordSuccess']);
+   Route::get('/register', [RegisterController::class, 'index']);
+   Route::post('/register', [RegisterController::class, 'register']);
 });
 
 // USER
 Route::middleware(['role:0'])->prefix('/user')->group(function() {
-    Route::get('/', [UserController::class, 'index']);
-    Route::get('/product-detail/{id}', [UserController::class, 'showProductDetail'])->name('productDetail');
-    Route::post('/product-detail/{id}', [UserController::class, 'handleUserAction']);
-    Route::get('/payment-confirmation/{id}', [UserController::class, 'paymentConfirmation'])->name('paymentConfirmation');
-    Route::delete('/payment-confirmation/{transactionDetail}', [UserController::class, 'cancelTransaction']);
-    Route::get('/payment-success', [UserController::class, 'paymentSuccess']);
-    Route::get('/favorite', [UserController::class, 'showFavorite']);
-    Route::delete('/product-detail/{id}', [UserController::class, 'removeFavorite']);
-    Route::delete('/favorite', [UserController::class, 'deleteFavorite']);
-    Route::get('/transaction', [UserController::class, 'showTransaction']);
-    Route::put('/transaction', [UserController::class, 'updateTransactionStatus']);
-    Route::get('/transaction/transaction-detail/{id}', [UserController::class, 'showTransactionDetail']);
-    Route::get('/profile', [UserController::class, 'showUserProfile']);
-    Route::get('/profile/update-profile', [UserController::class, 'editProfile']);
-    Route::put('/profile/update-profile', [UserController::class, 'updateProfile']);
-    Route::get('/profile/update-profile-success', [UserController::class, 'updateProfileSuccess']);
-    Route::get('/profile/change-password', [UserController::class, 'changePassword']);
-    Route::put('/profile/change-password', [UserController::class, 'updatePassword']);
-    Route::get('/profile/change-password-success', [UserController::class, 'updatePasswordSuccess']);
+   Route::get('/', [UserController::class, 'index']);
+   Route::get('/product-detail/{id}', [UserController::class, 'showProductDetail'])->name('productDetail');
+   Route::post('/product-detail/{id}', [UserController::class, 'handleUserAction']);
+   Route::get('/payment-confirmation/{id}', [UserController::class, 'paymentConfirmation'])->name('paymentConfirmation');
+   Route::delete('/payment-confirmation/{transactionDetail}', [UserController::class, 'cancelTransaction']);
+   Route::get('/payment-success', [UserController::class, 'paymentSuccess']);
+   Route::get('/favorite', [UserController::class, 'showFavorite']);
+   Route::delete('/product-detail/{id}', [UserController::class, 'removeFavorite']);
+   Route::delete('/favorite', [UserController::class, 'deleteFavorite']);
+   Route::get('/transaction', [UserController::class, 'showTransaction']);
+   Route::put('/transaction', [UserController::class, 'updateTransactionStatus']);
+   Route::get('/transaction/transaction-detail/{id}', [UserController::class, 'showTransactionDetail']);
+   Route::get('/profile', [UserController::class, 'showUserProfile']);
+   Route::get('/profile/update-profile', [UserController::class, 'editProfile']);
+   Route::put('/profile/update-profile', [UserController::class, 'updateProfile']);
+   Route::get('/profile/update-profile-success', [UserController::class, 'updateProfileSuccess']);
+   Route::get('/profile/change-password', [UserController::class, 'changePassword']);
+   Route::put('/profile/change-password', [UserController::class, 'updatePassword']);
+   Route::get('/profile/change-password-success', [UserController::class, 'updatePasswordSuccess']);
 });
 
 // ADMIN
 Route::middleware(['role:1'])->prefix('admin')->group(function() {
-    Route::get('/', [AdminController::class, 'index']);
-    Route::put('/', [AdminController::class, 'updateTransactionStatus']);
-    Route::get('/add-cake', [AdminController::class, 'addCake']);
-    Route::post('/add-cake', [AdminController::class, 'createCake']);
-    Route::get('/add-cake/add-cake-success', [AdminController::class, 'addCakeSuccess']);
-    Route::get('/edit-cake/{id}', [AdminController::class, 'editCake']);
-    Route::get('/update-cake/{id}', [AdminController::class, 'changeCake']);
-    Route::put('/update-cake/{cake}', [AdminController::class, 'updateCake']);
-    Route::get('/update-cake-success', [AdminController::class, 'updateCakeSuccess']);
-    Route::get('/delete-cake/{id}', [AdminController::class, 'deleteCakeConfirmation']);
-    Route::delete('/delete-cake/{cake}', [AdminController::class, 'deleteCake']);
-    Route::get('/delete-cake-success', [AdminController::class, 'deleteCakeSuccess']);
+   Route::get('/', [AdminController::class, 'index']);
+   Route::put('/', [AdminController::class, 'updateTransactionStatus']);
+   Route::get('/add-cake', [AdminController::class, 'addCake']);
+   Route::post('/add-cake', [AdminController::class, 'createCake']);
+   Route::get('/add-cake/add-cake-success', [AdminController::class, 'addCakeSuccess']);
+   Route::get('/edit-cake/{id}', [AdminController::class, 'editCake']);
+   Route::get('/update-cake/{id}', [AdminController::class, 'changeCake']);
+   Route::put('/update-cake/{cake}', [AdminController::class, 'updateCake']);
+   Route::get('/update-cake-success', [AdminController::class, 'updateCakeSuccess']);
+   Route::get('/delete-cake/{id}', [AdminController::class, 'deleteCakeConfirmation']);
+   Route::delete('/delete-cake/{cake}', [AdminController::class, 'deleteCake']);
+   Route::get('/delete-cake-success', [AdminController::class, 'deleteCakeSuccess']);
 });
 
